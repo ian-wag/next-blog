c2774f9c64e943625ef81782b9d99a46e549296f
iwagner76 <64107817+iwagner76@users.noreply.github.com>
2021-11-08 17:13:49 -0600

    spelling error

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,13 @@ export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
-        <title>blgr. | a place for Bloggers</title>
+        <title>blgr. | a place for bloggers</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => (
-            <PostCard post={post.node} key={post.tite} />
+            <PostCard post={post.node} key={post.titLe} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
